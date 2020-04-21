5786195f634bb4eefbfc9a46829e681cd46e26f5
hitenSharm <hiten.sharm1708@gmail.com>
2020-04-21 18:57:55 +0530

    Updates

---
+++ precache-manifest.6ff13fb0bc87ef1249b76aaf1b23ad8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e639fadd922525b8a1ac0313cb12d36b",
+    "url": "/Covid19Tracker/index.html"
+  },
+  {
+    "revision": "da495b55390527160f64",
+    "url": "/Covid19Tracker/static/css/main.a67c44a5.chunk.css"
+  },
+  {
+    "revision": "dd751e1a0762e822e03d",
+    "url": "/Covid19Tracker/static/js/2.ed9fecd4.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19Tracker/static/js/2.ed9fecd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da495b55390527160f64",
+    "url": "/Covid19Tracker/static/js/main.7800178c.chunk.js"
+  },
+  {
+    "revision": "fa899d9bc039f2bbea6c",
+    "url": "/Covid19Tracker/static/js/runtime-main.7cc38500.js"
+  }
+]);
